d04e6853a4e930fea6a90c43f4c66f9537652f5c
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-08-23 22:35:47 +0530

    Add files via upload

---
+++ RecursionLec1/m1.java
@@ -0,0 +1,17 @@
+package RecursionLec1;
+
+public class m1 {
+    static void prinIncreasing(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        prinIncreasing(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args){
+        prinIncreasing(5);
+
+    }
+
+}
---
+++ RecursionLec1/m2.java
@@ -0,0 +1,16 @@
+package RecursionLec1;
+
+public class m2 {
+    static void printDecreasing(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+    public static void main(String[] args) {
+        printDecreasing(5);
+    }
+}
