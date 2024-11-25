7836f7093c136139f7065352fc4055fe834b422e
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-11-25 22:23:46 +0530

    Add files via upload

---
+++ spaceCount.java
@@ -0,0 +1,12 @@
+//package Strings;
+//
+//public class spaceCount {
+//    public static void main(String[] args) {
+//        String str="Atul Panday";
+//        for(int i=0; i<str.length();i++){
+//            if(str.charAt(i).isEquals(" ")){
+//                System.out.println(str.split()+1);
+//            }
+//        }
+//    }
+//}
